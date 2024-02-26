ae0f9617dadba9be58f98c157ca5f341058e62fc
shepherdnick <shepherdnick@gmail.com>
2024-02-26 15:03:13 +0000

    WIP

--- ImageComparisonConsoleApplication/Program.cs
+++ ImageComparisonConsoleApplication/Program.cs
@@ -17,26 +17,29 @@ namespace ImageComparisonConsoleApplication
     {
         static int Main(string[] args)
         {
-            if (args.Length != 2)
-            {
-                Console.WriteLine("IMAGE COMPARISON CONSOLE APPLICATION");
-                Console.WriteLine("  by Jakob Farian Krarup, 2012");
-                Console.WriteLine("  Licensed under The Code Project Open License (CPOL)");
-                Console.WriteLine("  Compares two images and returns the difference in percent");
-                Console.WriteLine("    as an errorlevel (0 to 100)");
-                Console.WriteLine();
-                Console.WriteLine(@"  Usage: 'ImageComparisonConsole.exe [image1 path] [image2 path]");
-                Console.WriteLine(@"  Sample usage: 'ImageComparisonConsole.exe ""c:\image1.jpg"" ""c:\image2.bmp""");
-                return -1;
-            }
-            else
-            {
+            var imagePath1 = "/Users/nick/My Photos/2011/2011-10-8/img_1112_9327755523_o.jpg";
+            var imagePath2 = "/Users/nick/My Photos/2011/2011-10-8/img_1112_9327755523_o copy.jpg";
+
+            // if (args.Length != 2)
+            // {
+            //     Console.WriteLine("IMAGE COMPARISON CONSOLE APPLICATION");
+            //     Console.WriteLine("  by Jakob Farian Krarup, 2012");
+            //     Console.WriteLine("  Licensed under The Code Project Open License (CPOL)");
+            //     Console.WriteLine("  Compares two images and returns the difference in percent");
+            //     Console.WriteLine("    as an errorlevel (0 to 100)");
+            //     Console.WriteLine();
+            //     Console.WriteLine(@"  Usage: 'ImageComparisonConsole.exe [image1 path] [image2 path]");
+            //     Console.WriteLine(@"  Sample usage: 'ImageComparisonConsole.exe ""c:\image1.jpg"" ""c:\image2.bmp""");
+            //     return -1;
+            // }
+            // else
+            // {
                 //get, display and return the difference
-                int difference = (int)(ImageTool.GetPercentageDifference(args[0], args[1]) * 100);
+                int difference = (int)(ImageTool.GetPercentageDifference(imagePath1, imagePath2) * 100);
                 Console.WriteLine($"Comparing '{Path.GetFileName(args[0])}' and '{Path.GetFileName(args[1])}'");
                 Console.WriteLine($"The difference is {difference:0}%");
                 return difference;
-            }
+            //}
         }
     }
 }
